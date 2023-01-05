6dfd0af8bccbf52aeeaddd035514e5ea0ea332c7
Dahmani Mohammed Karim <109095748+Karim-Dahmani@users.noreply.github.com>
2023-01-05 01:19:04 +0100

    update commit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const cartRouter = require("./routes/cart");
+const orderRouter = require("./routes/order");
 
 dotenv.config();
 mongoose
@@ -16,7 +19,9 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
-
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/carts", cartRouter);
 app.listen(process.env.PORT || 5000, () => {
   console.log("backend server is running!");
 });
