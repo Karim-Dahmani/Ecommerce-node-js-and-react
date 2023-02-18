9bc5040d999d7fcce19d3b67fa7ca503e6a8ee03
Dahmani Mohammed Karim <109095748+Karim-Dahmani@users.noreply.github.com>
2023-02-18 18:50:22 +0100

    front

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,24 @@
-
-import React from 'react';
-
-import Announcement from '../Components/Announcement';
-import Categories from '../Components/Categories';
-import Slider from '../Components/Slider';
-import Navbar from '../Components/Navbar';
-import Products from '../Components/Products';
-import Footer from '../Components/Footer';
-import Newsletter from '../Components/Newsletter';
+import React from "react";
+import Announcement from "../Components/Announcement";
+import Categories from "../Components/Categories";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+import Newsletter from "../Components/Newsletter";
+import Products from "../Components/Products";
+import Slider from "../Components/Slider";
 
 const Home = () => {
-    return (
-        <div  className='container'>
-          
-           <Announcement/>
-           <Navbar/>
-           <Slider/>
-           <Categories/>
-           <Products/>
-           <Newsletter/>
-           <Footer/>
-           
-           
-         
-        </div>
-    );
-}
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
